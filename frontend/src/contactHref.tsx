18a68443a596518aabbdf1622eb9fe8bce2606bb
shobhitsaxena <ssaxena@jpischool.com>
2025-10-31 20:24:13 +0530

    Update contact information

--- frontend/src/contactHref.tsx
+++ frontend/src/contactHref.tsx
@@ -2,5 +2,5 @@ const subject = 'Hello, Vithal!';
 const body = 'I hope this note finds you well! I\'d love to connect about your work at SaathiCare, NightVisionX, or SynBioGuard.';
 
 export const contactHref = `mailto:vithalagrawal1@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-export const linkedInHref = 'https://www.linkedin.com/in/vithalagrawal/';
+export const linkedInHref = 'https://www.linkedin.com/in/vithal-agrawal/';
 export const twitterHref = 'https://twitter.com/vithalagrawal';
